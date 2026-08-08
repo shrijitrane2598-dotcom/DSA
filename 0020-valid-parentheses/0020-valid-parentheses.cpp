9f33682f37901c6702ac111b800731502d638972
Shrijit Rane <shrijitrane2598@gmail.com>
2026-08-08 12:16:29 +0530

    Time: 0 ms (100%), Space: 9 MB (38.16%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string str) {
+        stack<char> st;
+
+        for(int i = 0; i <str.size(); i++) {
+            if(str[i] == '(' || str[i] == '{' || str[i] == '[') {  // opening
+                st.push(str[i]);
+            } else {   // closing 
+                if(st.size() == 0) {
+                    return false;
+                }
+
+                if((st.top() == '(' && str[i] == ')') ||
+                (st.top() == '{' && str[i] == '}') ||
+                (st.top() == '[' && str[i] == ']')) {
+                    st.pop();
+                } else {  // no matcha
+                    return false;
+                }
+            }
+        } 
+        return st.size() == 0;
+    }
+};
